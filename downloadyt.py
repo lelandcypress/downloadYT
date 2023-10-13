be16aa04078c17d54c127df3bcd5b9d44edaa72e
Michael Souter <souterm1@gmail.com>
2023-10-13 14:15:46 -0400

    version 1.5, enables multiple downloads, creates directory

--- downloadyt.py
+++ downloadyt.py
@@ -1,19 +1,31 @@
 from pytube import YouTube
 import os
-def file_path():
+import asyncio
+ytPath = "YouTubeDownloads"
+def file_path(target):
     home = os.path.expanduser('~')
-    download_path = os.path.join(home, 'Downloads')
+    download_path = os.path.join(home, target)
     return download_path
 def Download(link):
     print("Download started")
     youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    youtubeObject = youtubeObject.streams.get_audio_only()
     try:
-        youtubeObject.download(file_path())
+        youtubeObject.download(file_path(ytPath))
         print("Download complete")
     except:
         print("An error has occurred")
+def getURLs():
+    uploadpath = "C:\\Users\\soute\\Documents\\youtubeclipsforDL.txt"
+    with open(uploadpath, "r") as f:
+        storeYTURL = [line for line in f]
+    for url in storeYTURL:
+        Download(url)
+
+if os.path.exists(ytPath):
+    getURLs()
+else:
+    os.makedirs(file_path(ytPath))
+    getURLs()
 
 
-link = input("Enter the YouTube video URL: ")
-Download(link)
